6a13b8ef4b025d1434e8a0d006566c49d411810a
Yu Wang <wangyu16@users.noreply.github.com>
2022-11-05 16:32:07 -0500

    Update phy_chem_constants.py

--- phy_chem_constants.py
+++ phy_chem_constants.py
@@ -1,2 +1,8 @@
 # Ideal gas constant
-Rgas = {'J/K mol':8.314462618, 'L atm/K mol':0.08205736608}
+R_gas = {'J/K mol':8.314462618, 'L atm/K mol':0.08205736608}
+# speed of light
+c_light = {'m/s':299792458.0, 'km/h':1079252848.8, 'km/s': 299792.458}
+# Planck's constant
+h_pl = {'m2 kg/s':6.62607015e-34}
+# Hydrogen ground state energy
+RH = {'J':-2.17987225e-18, 'eV':-13.605693} 
